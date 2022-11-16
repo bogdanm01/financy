c6b665e4722d83e545bd5d00cf7a44d5e3df60a4
Bogdan Milojević <bogi.milojevic@gmail.com>
2022-11-16 19:48:27 +0100

    implement login and register functionality

--- app/src/main/java/com/example/financy/SignInActivity.kt
+++ app/src/main/java/com/example/financy/SignInActivity.kt
@@ -5,22 +5,49 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import com.example.financy.databinding.ActivitySignInBinding
+import com.example.financy.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
-class SignInActivity : AppCompatActivity(), View.OnClickListener  {
-    private lateinit var txtSwitchToRegister: TextView;
+class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
 
-        txtSwitchToRegister = findViewById(R.id.login_bottom_text)
-        txtSwitchToRegister.setOnClickListener(this)
-    }
+        binding = ActivitySignInBinding.inflate(layoutInflater)
 
-    override fun onClick(p0: View?) {
-        when(p0?.id) {
-            R.id.login_bottom_text -> {startActivity(Intent(this, SignUpActivity::class.java))}
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        setContentView(binding.root)
+
+        binding.loginBottomText.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.idBtnLogin.setOnClickListener {
+            val email = binding.idEditEmail.text.toString()
+            val password = binding.idEditPassword.text.toString()
+
+            println("lol0")
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                println("lol1")
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        println("lol2")
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
-
 }
+
+
--- app/src/main/java/com/example/financy/SignUpActivity.kt
+++ app/src/main/java/com/example/financy/SignUpActivity.kt
@@ -6,13 +6,47 @@ import android.os.Bundle
 import android.view.View
 
 import android.widget.TextView
+import android.widget.Toast
+import com.example.financy.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.idBottomText.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.idBtnRegister.setOnClickListener {
+            val fullName = binding.idRegisterName.text.toString()
+            val email = binding.idRegisterEmail.text.toString()
+            val password = binding.idEditPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && fullName.isNotEmpty()) {
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, SignInActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            }
+        }
 
 
     }
