9bac0d14cf23d54458b85899edb2a64da4f90016
Bogdan Milojević <bogi.milojevic@gmail.com>
2022-12-08 14:08:06 +0100

    Implement detail activity

--- app/src/main/java/com/example/financy/DetailActivity.kt
+++ app/src/main/java/com/example/financy/DetailActivity.kt
@@ -1,9 +1,15 @@
 package com.example.financy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import com.example.financy.model.Transaction
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_detail.*
 import org.w3c.dom.Text
 
 class DetailActivity : AppCompatActivity() {
@@ -12,10 +18,39 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
 
         val transaction = intent.getParcelableExtra<Transaction>("transaction")
-        if(transaction != null) {
-            val textView : TextView = findViewById(R.id.test)
 
-            textView.text = transaction.name
+        if(transaction != null) { // set transaction data
+            transactionLabel.text = transaction.name;
+            transactionAmountValue.text = transaction.amount.toString();
+            transactionCategoryValue.text = transaction.category.toString();
+            transactionDateValue.text = formatDate(transaction.date.toString());
         }
+
+        deleteTransactionBtn.setOnClickListener() {
+//            deleteRecord(transaction?.transactionId!!)
+
+            TransactionRepository.deleteRecord(transaction?.transactionId!!).addOnSuccessListener {
+                Toast.makeText(this, "Transaction Deleted", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+//    private fun deleteRecord(id: String) {
+//        val dbRef = FirebaseDatabase.getInstance().getReference("Transactions").child(id);
+//        val mTask = dbRef.removeValue()
+//
+//        mTask.addOnSuccessListener {
+//            Toast.makeText(this, "Transaction Deleted", Toast.LENGTH_SHORT).show()
+//
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//        }
+//    }
+
+    private fun formatDate(date:String):String {
+        val array = date.split("-")
+        return array.reversed().joinToString(separator = "/")
     }
 }
